e3c83f7748188da76fc9859af14b7a7de6658d9d
Nail1551 <nail1551@yandex.ru>
2024-10-16 16:48:08 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
+        HashSet<Integer> numbers = new HashSet<>();
 
         System.out.println("Введите числа (0 для завершения):");
 
@@ -13,9 +13,7 @@ public class Main {
             if (num == 0) {
                 break;
             }
-            if (!numbers.contains(num)) {
-                numbers.add(num);
-            }
+            numbers.add(num);
         }
 
         System.out.println("Несовпадающие числа в последовательности:");
